08d37ac476d99d99efb6b0e01cb0e387b8fe53c4
Pascal Sauer <email@cubexs.dev>
2023-02-25 00:21:54 +0100

    Finish frontend python code

--- frontend/main.py
+++ frontend/main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+import requests
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def get_index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    response = requests.get("http://localhost:3000/api/words")
+    words = response.json()
+
+    context = {
+        "request": request,
+        "data":  words,
+    }
+    return templates.TemplateResponse("index.html", context)
