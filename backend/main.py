fae2daa3d61fea6aba12ed721abb6bd3ae6ba763
Pascal Sauer <email@cubexs.dev>
2023-02-25 00:22:07 +0100

    Set up basic backend code

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,38 @@
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+from pydantic import BaseModel
+
+# Testing
+word_list = []
+
+# Classes
+class Words(BaseModel):
+    list: List[str]
+
+# FastAPI Initialization
+origins = ["http://localhost:8080"]
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/api/words")
+path = "/api/words"
+
+# Declare paths
+@app.get(path)
 def get_list_of_words():
-    return { "value": "ABC" }
+    return word_list
+
+@app.post(path)
+def add_word_to_list(words: Words):
+    for word in words.list:
+        word_list.append({ "word": word, "length": len(word) })
+    return
